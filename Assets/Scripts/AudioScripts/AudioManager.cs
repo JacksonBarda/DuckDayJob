5ab289e8bc566042bd0bf810397a5169f093549e
Michael Maslowski <mmaslow@purdue.edu>
2023-11-17 16:05:28 -0500

    no message

---
+++ Assets/Scripts/AudioScripts/AudioManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Unity.VisualScripting;
+using UnityEditor;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    public Sound[] musicSounds, sfxSounds, dialogueSounds, movementSounds;
+    public AudioSource MusicSource, sfxSource, dialogueSource, movementSource;
+    private string duckname;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        PlayMusic("Lobby");
+    }
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x=> x.audioname == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+
+        else
+        {
+            MusicSource.clip = s.aclip;
+            MusicSource.Play();
+        }
+    }
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.audioname == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+
+        else
+        {
+            sfxSource.PlayOneShot(s.aclip);
+
+        }
+    }
+
+    public void PlayDialogue(string name)
+    {
+        Sound s = Array.Find(dialogueSounds, x => x.audioname == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+
+        else
+        {
+            
+            dialogueSource.PlayOneShot(s.aclip);
+              
+        }
+           
+    }
+    public void PlayMovement(string name)
+    {
+        Sound s = Array.Find(movementSounds, x => x.audioname == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound Not Found");
+        }
+
+        else
+        {
+            movementSource.clip = s.aclip;
+            movementSource.Play();
+        }
+    }
+}
