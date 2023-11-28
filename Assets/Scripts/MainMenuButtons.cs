70caba938c898c6fffac37b787e62f1fb42963b5
JBSPC\Jacks <jbarda@purdue.edu>
2023-11-28 00:49:25 -0500

    PlayerPrefs

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject MainMenu;
+    [SerializeField]
+    private GameObject SettingButton;
+    [SerializeField]
+    private GameObject NameMenu;
+
+    [SerializeField]
+    private TMP_InputField playerInput;
+
+    private void Start()
+    {
+       
+    }
+    public void OnStartClicked()
+    {
+        string _playerName = PlayerPrefs.GetString("playerName");
+        if (_playerName != null)
+        {
+            SceneManager.LoadScene("garrettsBlockout");
+        }
+        else
+        {
+            MainMenu.SetActive(false);
+            NameMenu.SetActive(true);
+        }
+        
+    }
+    public void OnSettingsClicked()
+    {
+
+    }
+    public void OnBackClicked()
+    {
+
+    }
+    public void OnEnterNameClicked()
+    {
+        PlayerPrefs.SetString("playerName", playerInput.text);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene("garrettsBlockout");
+    }
+    public void OnQuitClicked()
+    {
+        #if UNITY_EDITOR
+        // If running in the Unity editor
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            // If in play mode, exit play mode
+            UnityEditor.EditorApplication.ExitPlaymode();
+        }
+        #else
+            // If not running in the Unity editor, quit the application
+            Application.Quit();
+        #endif
+    }
+}
+
